3573603fddb7803c1a3178a0cfec2c98c909bf9a
Student <example@example.com>
2020-02-28 13:56:28 -0800

    creating stock app  for capstone.:

---
+++ src/Stock.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Stock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stockChartXValues: [],
+            stockChartYValues: []
+        }
+    }
+
+    componentDidMount(){
+        this.fetcStock();
+    }
+
+    fetchStock(){
+        const API_KEY = 'C5VYV510V7LZG320';
+        let API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&outputsize=full&apikey=${API_KEY}`;
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>
+                Stock
+                </h1>
+            </div>
+        )
+    }
+
+
+export default Stock;
