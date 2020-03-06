dfda725e91a7f32da012fd0b5900a5eca3a6828b
Student <example@example.com>
2020-03-06 09:01:37 -0800

    be in progress of creating user auth through firebase.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Stock from './components/Stock.js';
 import Crypto from './components/Crypto';
+import fire from './config/Fire';
 
-function App() {
-  return (
-    <div className="App">
-      <Stock/>
-      <Crypto/>
-    </div>
-  );
+class App extends Component() {
+  constructor(){
+    super(props);
+    this.state = {
+      user:{},
+    }
+  }
+
+
+ComponentDidMount(){
+  this.authListener();
+}
+
+authListener() {
+  fire.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if(user){
+      this.setState({user});
+      // localStorage.setItem('user' , user.uid);
+    } else {
+      this.setState({user: null});
+      // localStorage.removeItem('user');
+    }
+  });
+}
+
+  render(){
+    return (
+      <div className="App">
+        <Stock/>
+        <Crypto/>
+      </div>
+    );
+  }
 }
 
 export default App;
-ghfjk;
---
+++ src/config/Fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCQmxPp-p6qEoroaTZE29vE4d7IupDsgKc",
+    authDomain: "stok-29912.firebaseapp.com",
+    databaseURL: "https://stok-29912.firebaseio.com",
+    projectId: "stok-29912",
+    storageBucket: "stok-29912.appspot.com",
+    messagingSenderId: "651289063363",
+    appId: "1:651289063363:web:e567393f3c211d43295776",
+    measurementId: "G-GR8P4B95FV"
+  };
+  const fire = firebase.initializeApp(config);
+
+  
+  export default fire;
