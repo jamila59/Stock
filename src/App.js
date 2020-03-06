fa683dac500fd8596e050a7c5bdd50f9942d3499
Student <example@example.com>
2020-03-06 11:32:13 -0800

    debugging firebase auth.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-import Stock from './components/Stock.js';
-import Crypto from './components/Crypto';
-import fire from './config/Fire';
+// import Stock from './components/Stock.js';
+// import Crypto from './components/Crypto';
 import Login from './components/Login';
 import Home from './components/Home';
+import fire from './config/Fire';
 
-class App extends Component() {
-  constructor(){
-    super(props);
-    this.state = {
-      user:{},
-    }
+class App extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
   }
 
 
-ComponentDidMount(){
+componentDidMount(){
   this.authListener();
 }
 
 authListener() {
   fire.auth().onAuthStateChanged((user) => {
     console.log(user);
-    if(user){
-      this.setState({user});
+    if (user) {
+      this.setState({ user });
+      localStorage.setItem('user', user.uid);
     } else {
-      this.setState({user: null});
+      this.setState({ user: null });
+      localStorage.removeItem('user');
     }
   });
 }
@@ -34,8 +37,8 @@ authListener() {
     return (
       <div className="App">
         {this.state.user ? (<Home/>) : (<Login/>)}
-        <Stock/>
-        <Crypto/>
+        {/* <Stock/>
+        <Crypto/> */}
       </div>
     );
   }
